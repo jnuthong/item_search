1457849172bac662fd261c7392bfc4c44241ff7d
jnuthong <jnuthong@gmail.com>
2014-10-23 20:20:54 +0800

    add iterator/multiChannel

--- utils/iterator/iterator.go
+++ utils/iterator/iterator.go
@@ -5,28 +5,43 @@ import (
 )
 
 type Element struct{
+	id string
 	key string
 	value interface{}
-	type string
-	label string
+	datatype string
+	label string	// maintain for further using
+	parent string
+	path string
 }
 
-func GeneElement(key string, value interface{}, type string, label string) *Element{
-	i := Element{key: key, value: value, type: type, label: label}
+type Child struct{
+	id string
+	name string
+	label string
+} 
+
+type Iterator struct{
+	Next() *Element
+	Previous() *Element
+}
+
+func GeneElement(key string, value interface{}, datatype string, label string) *Element{
+	i := Element{key: key, value: value, datatype: datatype, label: label}
 	return &i
 }
 
-func (opt *Element) GetElementAttribute(key string) value interface{} {
+func (opt *Element) GetElementAttribute(key string) interface{} {
 	if key == "key"{
 		return opt.key
 	}
 	if key == "value"{
 		return opt.value
 	}
-	if key == "type"{
-		return opt.type
+	if key == "datatype"{
+		return opt.datatype
 	}
 	if key == "label"{
 		return opt.label
 	}
+	return errors.News("[error] Couldnt find the relevant key: " + key)
 }
